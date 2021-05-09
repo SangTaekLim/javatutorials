7bcda7229a4b2bb2dcabde930d1227b03ba7d3e2
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-05-09 17:34:38 +0900

    입출력테스트

---
+++ src/org/opentutorials/javatutorials/io/InputDemo.java
@@ -0,0 +1,7 @@
+package org.opentutorials.javatutorials.io;
+
+public class InputDemo {
+	public static void main(String[] args) {
+		System.out.println(args.length);
+	}
+}
---
+++ src/org/opentutorials/javatutorials/io/ScannerDemo.java
@@ -0,0 +1,15 @@
+package org.opentutorials.javatutorials.io;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		System.out.println(i*1000);
+		sc.close();
+
+	}
+
+}
