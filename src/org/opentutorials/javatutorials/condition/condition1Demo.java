f2882f18a43a4a5303cf8eeb5034443c8af9d775
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-05-09 15:29:56 +0900

    조건문

---
+++ src/org/opentutorials/javatutorials/condition/condition1Demo.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorials.condition;
+
+public class condition1Demo {
+
+	public static void main(String[] args) {
+		if(true) {
+			System.out.println("123");
+		}
+
+	}
+
+}
