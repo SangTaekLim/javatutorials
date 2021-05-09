76008dcacdd583c5967b7026af0c8435ce15b562
DESKTOP-9JAI4JE\임상택 <lst0415@daum.net>
2021-05-09 15:21:42 +0900

    변수

---
+++ src/org/opentutorials/javatutorials/numberstring/CharString.java
@@ -0,0 +1,9 @@
+package org.opentutorials.javatutorials.numberstring;
+
+public class CharString {
+
+	public static void main(String[] args) {
+		System.out.println("egoing \n said ");
+	}
+
+}
---
+++ src/org/opentutorials/javatutorials/variables/IntegerDemo.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.variables;
+
+public class IntegerDemo {
+	public static void main(String[] args) {
+	
+		String a = "hello";
+		String b = new String("hello");
+		
+		System.out.println(a.equals(b));
+	}
+}
